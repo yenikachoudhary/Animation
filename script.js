b1a2eaa8cff888e05ef91ec71b333a6ad8ef52e5
yenikachoudhary <yenikachoudhary1207@gmail.com>
2026-09-05 12:13:34 +0530

    Adding ground

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let ground = canvas.height - 80;
+let grass = canvas.height - 100;
+let snow = canvas.height - 110;
 
 let snowflakes = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 200; i++) {
     let snowflake = {
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
@@ -16,23 +19,35 @@ for (let i = 0; i < 100; i++) {
     snowflakes.push(snowflake);
 }
 
-function animate() {
+function drawGround() {
+    ctx.fillStyle = "#914514";
+    ctx.fillRect(0, ground, canvas.width, canvas.height - ground);
+
+    ctx.fillStyle = "#7CFC00"; 
+    ctx.fillRect(0, grass, canvas.width, ground - grass); 
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,snow, canvas.width,grass - snow);
+}
+
+function snowfall() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < snowflakes.length; i++) {
         let snowflake = snowflakes[i];
         snowflake.y = snowflake.y + snowflake.speed;
 
-    if (snowflake.y > canvas.height) {
-        snowflake.y = 0;
-    }
-    ctx.beginPath();
+        if (snowflake.y > snow) {
+            snowflake.y = 0;
+        }
+        ctx.beginPath();
 
-        ctx.arc(snowflake.x,snowflake.y,snowflake.radius,0,Math.PI * 2);
+        ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2);
 
         ctx.fillStyle = "white";
         ctx.fill()
     }
-    requestAnimationFrame(animate);
+    drawGround();
+    requestAnimationFrame(snowfall);
 }
+snowfall();
 
-animate();
